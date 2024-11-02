18932cbd90aff3afe81143a5c252c20ba9366874
Azhar Khan <157284598+Azharkhan0786@users.noreply.github.com>
2024-11-02 23:52:04 +0530

    Add files via upload

---
+++ primenumbersbetweentwono.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class primenumbersbetweentwono {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the lowest number");
+        int low = sc.nextInt();
+        System.out.println("enter the highest number");
+        int high = sc.nextInt();
+        if(low==1){
+            System.out.println("not a prime not a composite number");
+        }
+
+        for (int n = low; n <= high; n++) {
+            int count = 0;
+
+            for (int div = 2; div * div <= n; div++) {
+                if (n % div == 0) {
+                    count++;
+                    break;
+
+                }
+            }
+            if (count == 0) {
+                System.out.println(n);
+            }
+        }
+    }
+
+}
